a179e243169827e102023442e83873ed289f1b8f
Mateusz Bielawski <mateusz.bielawski@dektech.com.au>
2017-10-11 02:51:14 +0200

    list of list finished and tested

--- src/matrix_as_list_of_lists.erl
+++ src/matrix_as_list_of_lists.erl
@@ -8,25 +8,28 @@
 %%%-------------------------------------------------------------------
 -module(matrix_as_list_of_lists).
 
-%% Application callbacks
--export([rows_sums/1, cols_sums/1]).
+-export([rows_sums/1, cols_sums/1,
+         get_value/3,set_value/4]).
 
-%%%===================================================================
-%%% Application callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%%--------------------------------------------------------------------
 rows_sums(Matrix) ->
     [lists:sum(Row)|| Row <- Matrix].
 
 cols_sums([])->[];
 cols_sums(Matrix)->cols_sums(Matrix,[]).
 
-cols_sums([[]|_],Acc)->
+cols_sums([[]|_], Acc)->
      lists:reverse(Acc);
-cols_sums(Matrix,Acc) ->
-    {Heads,Tails} = lists:unzip([{Head,Tail}|| [Head|Tail]<- Matrix]),   
-    cols_sums(Tails,[lists:sum(Heads)|Acc]).
+cols_sums(Matrix, Acc) ->
+    {Heads, Tails} = lists:unzip([{Head,Tail} || [Head| Tail]<- Matrix]),   
+    cols_sums(Tails, [lists:sum(Heads)| Acc]).
 
-
+get_value(X, Y, Matrix)->
+    Row = lists:nth(Y, Matrix),
+    lists:nth(X, Row).
+    
+set_value(X, Y, NewValue, Matrix)->
+    {PrecedingList,[Row | TailList]} = lists:split(Y-1, Matrix),
+    {PrecedingElements,[OldValue | TailElements]} = lists:split(X-1,Row),
+    lists:append([PrecedingList,
+                 [lists:append([PrecedingElements,[NewValue], TailElements])],
+                 TailList]).
